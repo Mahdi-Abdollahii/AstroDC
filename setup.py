efdf6ebe4ddba704096d1c112f12bfed24a5c47f
Mahdi <50545417+mahdiabdollahii96@users.noreply.github.com>
2022-11-13 20:15:11 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
-    name='Data_Claening',
+    name='AstroDC',
     version='0.0.1',    
     description='A simple Python package helps us to clean data',
     url='https://github.com/mahdiabdollahii96',
     author='Mahdi',
     author_email='mahdiabdollahii96@gmail.com',
     license='MIT',
-    packages=['Data_Claening'],
+    packages=['AstroDC'],
     install_requires=required,
     package_data={},
     include_package_data=False,

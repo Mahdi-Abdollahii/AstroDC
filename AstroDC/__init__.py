494fa3fadac48da0ff7017cf7f53ad4748bff789
Mahdi <50545417+Mahdi-Abdollahii@users.noreply.github.com>
2022-11-28 18:15:00 +0330

    Update __init__.py

--- AstroDC/__init__.py
+++ AstroDC/__init__.py
@@ -79,7 +79,7 @@ class AstroDC:
              Units = (units.hourangle, units.deg) ,
              Color = ("white", "black", "red", "green", "blue", "cyan", "magenta", "yellow") ,
              Size = 5 ,
-             File_name = 'reg_file' ):
+             File_name = 'reg_file'):
       '''
         This function provides a reg file that helps you to illustrate the targets in the AstroDs9 application.
 
@@ -97,7 +97,7 @@ class AstroDC:
 
       # making raw file and fill it by region data 
       f = open( File_name + '.reg' , "a")
-      f.write('global color=' + Color +' font="helvetica 10 normal" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source'+'\n')
+      f.write('global color=' + Color + ' font="helvetica 10 normal" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source' + '\n')
 
       for i in list_cor_deg :
         Ra_deg  = str(i).split('deg\n')[1].split('(')[1].split(')')[0].split(',')[0]
